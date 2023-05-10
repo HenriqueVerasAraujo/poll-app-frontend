51d6ff6c7e1b98185325a320ca1f396e1fb63cea
Henrique_Veras <theveras1@gmail.com>
2023-05-10 18:26:05 -0300

    Transfering all functions to SinglePollComponent and allPollSection

--- src/components/AllPollsSection.jsx
+++ src/components/AllPollsSection.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import SinglePoll from './common/SinglePoll';
 
 export default function AllPollsSection({ pollsInfo }) {
+  const [renderButton, setRenderButton] = useState(false);
+
+  useEffect(() => {
+    verifyUserId();
+  }, []);
+  
+  const verifyUserId = () => {
+    const userIdFromLocal = localStorage.getItem('userId');
+    if (!userIdFromLocal) {
+      return setRenderButton(false);
+    };
+    if (userIdFromLocal === pollsInfo.userId) {
+      setRenderButton(true);
+    } else {
+      setRenderButton(false);
+    };
+  };
+
   return (
     <div>
         {pollsInfo.map((singlePoll) => (
             <div>
+              <SinglePoll pollInfo={singlePoll} />
                 <h1>{singlePoll.pollTitle}</h1>
                 <h1>{singlePoll.createdAt}</h1>
             </div>
         ))}
     </div>
   )
-}
+};
---
+++ src/components/common/SinglePoll.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SinglePoll({ pollInfo }) {
+  return (
+    <div>SinglePoll</div>
+  )
+}
